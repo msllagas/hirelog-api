24ca7cfc1b50746fe1b3629baa820eb9c8ef5c3c
msllagas <mndyllagas@gmail.com>
2025-05-13 21:45:05 +0800

    feat(currency): add model, migration, factory, and seeder

---
+++ app/Models/Currency.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\CurrencyFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    /** @use HasFactory<CurrencyFactory> */
+    use HasFactory;
+
+}
---
+++ database/factories/CurrencyFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Currency;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Currency>
+ */
+class CurrencyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'code' => strtoupper(fake()->unique()->currencyCode()),
+            'name' => fake()->country(),
+        ];
+    }
+}
