3074a8dc32dc3371b7ca8f4b671d480e98bc29b5
msllagas <mndyllagas@gmail.com>
2025-05-14 10:43:28 +0800

    chore(db): register ApplicationStatusSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CurrencySeeder::class,
             EmploymentTypeSeeder::class,
+            ApplicationStatusSeeder::class,
         ]);
     }
 }
