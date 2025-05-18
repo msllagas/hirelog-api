a69b35f07e978f6bcd18af84539a6a37e7cd126b
msllagas <mndyllagas@gmail.com>
2025-05-18 17:50:18 +0800

    chore(ide-helper): add PHPDoc annotations to JobType model

--- app/Models/JobType.php
+++ app/Models/JobType.php
@@ -3,9 +3,30 @@
 namespace App\Models;
 
 use Database\Factories\JobTypeFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property string $name E.G., Full-Time, Part-Time, Contract, Freelance, Internship, Temporary
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $deleted_at
+ *
+ * @method static JobTypeFactory factory($count = null, $state = [])
+ * @method static Builder<static>|JobType newModelQuery()
+ * @method static Builder<static>|JobType newQuery()
+ * @method static Builder<static>|JobType query()
+ * @method static Builder<static>|JobType whereCreatedAt($value)
+ * @method static Builder<static>|JobType whereDeletedAt($value)
+ * @method static Builder<static>|JobType whereId($value)
+ * @method static Builder<static>|JobType whereName($value)
+ * @method static Builder<static>|JobType whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
 class JobType extends Model
 {
     /** @use HasFactory<JobTypeFactory> */
