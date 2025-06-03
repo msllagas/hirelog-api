9e74b69c900538d143c3a81bdfa6af7f446e527b
msllagas <mndyllagas@gmail.com>
2025-06-03 20:14:41 +0800

    feat(api): add job-related routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\SignUpController;
+use App\Http\Controllers\Job\ApplicationStatusController;
+use App\Http\Controllers\Job\JobApplicationController;
+use App\Http\Controllers\Job\JobTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\SignUpController;
-use App\Http\Controllers\Auth\LogoutController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,3 +21,11 @@ Route::get('/hello', function () {
 Route::post('login', LoginController::class)->middleware('guest');
 Route::post('signup', SignupController::class)->middleware('guest');
 Route::post('logout', LogoutController::class)->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResources([
+        'job-applications' => JobApplicationController::class,
+        'job-types' => JobTypeController::class,
+        'application-statuses' => ApplicationStatusController::class,
+    ]);
+});
