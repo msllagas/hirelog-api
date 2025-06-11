80e7a4b6cdf877b94dc9e7ec609ced9d51081b47
msllagas <mndyllagas@gmail.com>
2025-06-11 20:39:08 +0800

    feat(job-application): implement index method

--- app/Http/Controllers/Job/JobApplicationController.php
+++ app/Http/Controllers/Job/JobApplicationController.php
@@ -5,16 +5,24 @@ namespace App\Http\Controllers\Job;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Job\StoreJobApplicationRequest;
 use App\Http\Requests\Job\UpdateJobApplicationRequest;
+use App\Http\Resources\JobApplicationResource;
 use App\Models\JobApplication;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Auth;
 
 class JobApplicationController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
-        //
+        $jobApplications = JobApplication::whereUserId(Auth::id())
+            ->with(['jobType', 'applicationStatus'])
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+
+        return JobApplicationResource::collection($jobApplications);
     }
 
     /**
