cb6f20e43489cff27bd5f0a2599aedefa42b15fb
msllagas <mndyllagas@gmail.com>
2025-06-14 08:52:10 +0800

    chore(job-application): create migration with initial tables

---
+++ database/migrations/2025_05_11_021239_create_job_applications_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\ApplicationStatus;
+use App\Models\JobType;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_applications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignIdFor(JobType::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('company_name');
+            $table->string('position');
+            $table->string('location', 50)->nullable();
+            //            $table->longText('description')->nullable();
+            $table->foreignIdFor(ApplicationStatus::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            //            $table->string('application_url')->nullable();
+            //            $table->timestamp('applied_at');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_applications');
+    }
+};
