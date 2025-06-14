e79050b832c6e8f2f7e39631a29e47bbb2edae49
msllagas <mndyllagas@gmail.com>
2025-06-14 09:17:23 +0800

    feat(job-application): add store validation form request

--- app/Http/Requests/Job/StoreJobApplicationRequest.php
+++ app/Http/Requests/Job/StoreJobApplicationRequest.php
@@ -12,7 +12,7 @@ class StoreJobApplicationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,35 @@ class StoreJobApplicationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'user_id' => 'required|integer|numeric|exists:users,id',
+            'company_name' => 'required|string|max:255',
+            'job_type_id' => 'required|integer|numeric|exists:job_types,id',
+            'position' => 'required|string|max:255',
+            'application_status_id' => 'required|integer|numeric|exists:application_statuses,id',
+            'location' => 'nullable|string|max:50',
+            'description' => 'nullable|string|max:255',
+            //            'application_url' => 'required|string|max:255',
+        ];
+    }
+
+    public function prepareForValidation(): void
+    {
+        $this->merge([
+            'user_id' => \Auth::id(),
+        ]);
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'job_type_id' => 'job type',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'job_type_id.required' => 'The :attribute is required.',
         ];
     }
 }
